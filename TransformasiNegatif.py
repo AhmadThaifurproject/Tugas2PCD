b87ab38092db834ac70e8f0eef7e5b12742acedf
AhmadThaifurproject <145728401+AhmadThaifurproject@users.noreply.github.com>
2024-04-16 13:58:23 +0800

    Add files via upload

---
+++ TransformasiNegatif.py
@@ -0,0 +1,19 @@
+import cv2
+
+img = cv2.imread(r"C:\Users\ASUS\PycharmProjects\Belajar Python\img\mammogram.jpg", 0)
+img_1 = 255 - img
+
+cv2.imshow("Original Image", img)
+cv2.imshow("Image Negative", img_1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
